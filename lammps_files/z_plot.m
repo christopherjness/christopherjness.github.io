c8c0ac8326144da9c88492927b1d50625412293e
christopherjness <14129181+christopherjness@users.noreply.github.com>
2021-07-20 16:04:39 +0100

    Lammps update

--- lammps_files/z_plot.m
+++ lammps_files/z_plot.m
@@ -2,24 +2,27 @@
 data = read_log(['log.lammps'],1)
  
  
- strain =  0.001*data.data(2:end,1);
- stress = -data.data(2:end,5)/(0.1*0.001);
+ strain =  0.01*data.data(2:end,1);
+ stress = -data.data(2:end,5)/(0.1*0.01);
  
  
  
 figure
 box on; hold on;
+pbaspect([1 1 1])
 %set(gca, 'XScale', 'log')
-%set(gca, 'YScale', 'log')
+set(gca, 'YScale', 'log')
 set(gca,'Layer','top')
 %xlim([0 1])
-%ylim([0.1 4]);
+ylim([1 200]);
 set(gca,'LineWidth',2,'FontSize',30);
-xlabel('Strain $\dot{\gamma}t$','FontSize',30,'interpreter','latex')
-ylabel('Viscosity $\sigma_{xy}/\eta\dot{\gamma}$','FontSize',30,'interpreter','latex')
+%xlabel('Strain $\dot{\gamma}t$','FontSize',30,'interpreter','latex')
+%ylabel('Viscosity $\sigma_{xy}/\eta_\mathrm{f}\dot{\gamma}$','FontSize',30,'interpreter','latex')
 
 
 xxx = strain;
 yyy = stress;
-plot(xxx,yyy,'.-','linewidth',2);
+plot(xxx,yyy,'ok-','linewidth',2);
+print('~/Desktop/Fig2','-depsc')
+
 
