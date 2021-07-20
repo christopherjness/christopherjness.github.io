f84ea88ac43636f63d5b747f481ff82a19718cab
christopherjness <14129181+christopherjness@users.noreply.github.com>
2021-07-20 14:18:02 +0100

    Lammps page updates

--- lammps_files/z_plot.m
+++ lammps_files/z_plot.m
@@ -2,8 +2,8 @@
 data = read_log(['log.lammps'],1)
  
  
- strain =  0.01*data.data(2:end,1);
- stress = -data.data(2:end,5)/(0.1*0.01);
+ strain =  0.001*data.data(2:end,1);
+ stress = -data.data(2:end,5)/(0.1*0.001);
  
  
  
@@ -16,10 +16,10 @@ set(gca,'Layer','top')
 %ylim([0.1 4]);
 set(gca,'LineWidth',2,'FontSize',30);
 xlabel('Strain $\dot{\gamma}t$','FontSize',30,'interpreter','latex')
-ylabel('Viscosity $\sigma/\eta\dot{\gamma}$','FontSize',30,'interpreter','latex')
+ylabel('Viscosity $\sigma_{xy}/\eta\dot{\gamma}$','FontSize',30,'interpreter','latex')
 
 
 xxx = strain;
 yyy = stress;
-plot(xxx,yyy,'-o','linewidth',2);
+plot(xxx,yyy,'.-','linewidth',2);
 
